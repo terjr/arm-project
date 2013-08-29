714d113f183c156adcb5de06337053291445f3ac
Terje Runde <terje.runde@gmail.com>
2013-08-29 15:01:52 +0200

    Disable all interrupts when loading kernel module.

--- kmodules/base.c
+++ kmodules/base.c
@@ -1,4 +1,5 @@
 #include <linux/init.h>
+#include <linux/irqflags.h>
 #include <linux/module.h>
 
 MODULE_LICENSE("GPL");
@@ -7,7 +8,15 @@ void runtest(void);
 
 static int hello_init(void) {
     printk(KERN_ALERT "Hello, world\n");
+
+    unsigned long irqs;
+    local_irq_save(irqs);
+
+    printk("Interrupts enabled: 0x%X\n", irqs);
+
     runtest();
+
+    local_irq_restore(irqs);
     return 0;
 }
 
