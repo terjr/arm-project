244130b176e87a829ed15aadd5358bfb14d65544
= <=>
2013-09-19 14:33:12 +0200

    Plotting: Histogram with boxerrorbars that works.

--- scripts/generate_results.sh
+++ scripts/generate_results.sh
@@ -13,6 +13,7 @@ echo "set style fill solid 1.00 border 0"
 echo "set style histogram errorbars gap 2 lw 1"
 
 #echo "set boxwidth 1"
+#echo "set boxwidth -2.0"
 #echo "set style fill empty"
 #echo "set style line 2"
 echo "set yrange [0:0.8]"
@@ -20,17 +21,17 @@ echo "set xrange [-0.5:`echo ${FILELIST} | wc -w `]"
 echo "set xtics rotate by -45"
 echo
 
-echo "plot '-' using 1:2:3:4:xtic(6) with boxerrorbars"
+echo "plot '-' using 1:2:3:4:5:xtic(6) with boxerrorbars"
 for LOG_FILE in ${FILELIST}
 do
     if [ -z `echo ${LOG_FILE} | grep -v microarch` ]
     then
         continue
     fi
-    F=`basename $(echo ${LOG_FILE} | sed -e 's/log$//')`
-    AVG=`awk 'BEGIN {x=0; min=100000; max=0} $2 > 0 {sum+=$2; x+=1} $2 > max {max=$2} $2 < min {min=$2}  END {avg=sum/x; print avg - min, max - avg}' $LOG_FILE`
-    MEDIAN=`awk '{print $2+0}' $LOG_FILE | sort -n -k1 | awk 'NR==25000 {print $1}'`
-    echo ${COUNTER} ${MEDIAN}  ${AVG} -2 ${F}
+    F=$(basename $(echo ${LOG_FILE} | sed -e 's/log$//'))
+    MIN_MAX=$(awk 'BEGIN {min=100000} {$2 = gensub("A","","g",$2);} $2 > 0 {sum+=$2; x+=1} $2 > max {max=$2} $2 < min {min=$2} END {print min, max}' $LOG_FILE)
+    MEDIAN=$(awk '{print $2+0}' $LOG_FILE | sort -n -k1 | awk 'NR==25000 {print $1}')
+    echo ${COUNTER} ${MEDIAN} ${MIN_MAX} 0.5 ${F}
     let COUNTER=COUNTER+1
 done
 
