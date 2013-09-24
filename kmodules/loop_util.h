ba89b37ae2dd3ceb899364b46ae678e5896da8a4
Terje Runde <terje.runde@gmail.com>
2013-09-24 09:48:56 +0200

    Utility defines for fast-loop.

---
+++ kmodules/loop_util.h
@@ -0,0 +1,41 @@
+#define DISABLE_L1_CACHE() \
+    __asm__ volatile ( \
+            "mrc p15, 0, r0, c1, c0, 0\n" \
+            "bic r0, r0, #(0x1 << 12)\n" \
+            "mcr p15, 0, r0, c1, c0, 0\n" \
+            );
+
+#define INIT_ITER_COUNT() \
+    __asm__ volatile ( \
+            "mov r7, #0x1fffffff\n" \
+            );
+
+#define INIT_RANDOM_REGVALS() \
+    __asm__ volatile ( \
+            "mov r0, #0x55\n" \
+            "mov r1, #0x71\n" \
+            "mov r2, #0x11\n" \
+            "mov r3, #0x01\n" \
+            );
+
+#define LOOP_HEAD(label) \
+    __asm__ volatile ( \
+            ".balign 64\n" \
+            label":\n" \
+            );
+
+#define LOOP_TAIL(label) \
+    __asm__ volatile ( \
+            "subs r7, r7, #1\n" \
+            "bne label\n" \
+            );
+
+#define RESET_CYCLE_COUNTER() \
+    __asm__ volatile ("mcr p15, 0, %0, c9, c12, 0\n" :: "r"(0x17)); \
+    __asm__ volatile ("mcr p15, 0, %0, c9, c12, 3\n" :: "r"(0x8000000f)); \
+    __asm__ volatile ("mcr p15, 0, %0, c9, c12, 1\n" :: "r"(0x8000000f));
+
+#define READ_CYCLE_COUNTER(variable) \
+    __asm__ volatile ( \
+            "mrc p15, 0, %0, c9, c13, 0\n": "=r"(variable) \
+            );
