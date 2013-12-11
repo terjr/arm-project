f8442806529910f60d806ef2deb1c7c3fa457ff1
= <=>
2013-12-11 13:48:41 +0100

    results with SD

--- scripts/avg_medians.sh
+++ scripts/avg_medians.sh
@@ -13,7 +13,25 @@ cat $DIR/../.tests | while read LINE;
 do
     INSTR=$(echo $LINE | sed 's|.*/\(.*\)|\1|')
     echo -n "$INSTR "
-    $DIR/results_summary.sh $INSTR $RESULTS_DIR \
-        | sed 's|,| |' \
-        | awk '{sum += $2; count += 1} END {print sum/count}'
+    $DIR/results_summary.sh $INSTR $RESULTS_DIR | awk '{print $3}' |
+        perl -e 'use List::Util qw(max min sum);
+    @a=();
+    while(<>){
+        $sqsum+=$_*$_;
+        push(@a,$_)
+    };
+    $n=@a;
+    $s=sum(@a);
+    $a=$s/@a;
+    $m=max(@a);
+    $mm=min(@a);
+    $std=sqrt($sqsum/$n-($s/$n)*($s/$n));
+    $mid=int @a/2;
+    @srtd=sort @a;
+    if(@a%2){
+        $med=$srtd[$mid];
+    }else{
+    $med=($srtd[$mid-1]+$srtd[$mid])/2;
+};
+print "$a $std\n";'
 done
--- scripts/results_summary.sh
+++ scripts/results_summary.sh
@@ -2,30 +2,53 @@
 DIR=$(dirname $(readlink -f $0))
 RESULTS_DIR=$DIR/../old_results
 
-function FIND_MEDIAN()
+function FIND_STATS()
 {
+  awk '{print $2+0}' $1 |
+  perl -e 'use List::Util qw(max min sum);
+  @a=();
+  while(<>){
+      $sqsum+=$_*$_;
+      push(@a,$_)
+  };
+  $n=@a;
+  $a=$s/@a;
+  $mid=int @a/2;
+  @srtd=sort @a;
+  if(@a%2){
+      $med=$srtd[$mid];
+  }else{
+  $med=($srtd[$mid-1]+$srtd[$mid])/2;
+      };
+      print "$med";'
+  }
 
-  TEST_NAME=$(echo $1 | sed 's|.*/\([^/]*\)/.*\.\([^/]*\)/.*|\1,\2|')
-  MIDDLE=$(($(cat $1 | wc -l) / 2))
-  cat $1 | tr -d 'A' | sort -n -k 2 | awk "NR == $MIDDLE {print \"$TEST_NAME\" \",\" \$2}"
-}
+  function FIND_MEDIAN()
+  {
+
+      TEST_NAME=$(echo $1 | sed 's|.*/\([^/]*\)/.*\.\([^/]*\)/.*|\1 \2|')
+      MIDDLE=$(($(cat $1 | wc -l) / 2))
+      echo $(cat $1 | tr -d 'A' | sort -n -k 2 | awk "NR == $MIDDLE {print \"$TEST_NAME\" \" \" \$2}")
+  }
 
 
-INSTR=$1
-if [ -z $INSTR ]
-then
-    echo "Please provide instruction as argument"
-fi
+  INSTR=$1
+  if [ -z $INSTR ]
+  then
+      echo "Please provide instruction as argument"
+  fi
 
-if [ ! -z $2 ]
-then
-    RESULTS_DIR=$2
-fi
+  if [ ! -z $2 ]
+  then
+      RESULTS_DIR=$2
+  fi
 
 
-FILES=$(find $RESULTS_DIR -path "*/$INSTR/log")
+  FILES=$(find $RESULTS_DIR -path "*/$INSTR/log")
 
-for F in $FILES
-do
-    FIND_MEDIAN $(readlink -f $F)
-done
+  for F in $FILES
+  do
+      #TEST_NAME=$(echo $F | sed 's|.*/\([^/]*\)/.*\.\([^/]*\)/.*|\1,\2|')
+      FIND_MEDIAN $(readlink -f $F)
+      #    echo $MEDIAN $SD
+  done
